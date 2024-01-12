72ec5a5f3034d22a705915a43f1660c103fc0247
Drew Reed <drew.reed@hotmail.com>
2024-01-12 15:18:38 -0600

    removed weather to own app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
-import { TodayComponent } from './today/today.component';
-import { FutureComponent } from './future/future.component';
+
 
 const routes: Routes = [
   {path: '', component:HomepageComponent},
-  {path: 'today', component:TodayComponent},
-  {path: 'future', component:FutureComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { HttpClientModule } from '@angular/common/http';
-import { TodayComponent } from './today/today.component';
-import { FutureComponent } from './future/future.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomepageComponent,
-    TodayComponent,
-    FutureComponent
     ],
   imports: [
     BrowserModule,
