981ee4a709794332a11f421b535278c463f28886
Drew Reed <drew.reed@hotmail.com>
2024-01-27 10:53:27 -0600

    add required for contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -13,13 +13,16 @@ export class ContactComponent implements OnInit {
     this.buildForm();
   }
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) {}
+  constructor(private formBuilder: FormBuilder) {}
 
   send(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched(); // Mark all fields as touched to show error messages
+      return;
+    }
+
     const { name, email, message } = this.form.value;
-    const endpoint = "https://2mkop4gz3e.execute-api.us-east-2.amazonaws.com/default/sendContactEmail"
+    const endpoint = "https://2mkop4gz3e.execute-api.us-east-2.amazonaws.com/default/sendContactEmail";
     const body = JSON.stringify({
       senderName: name,
       senderEmail: email,
@@ -31,25 +34,24 @@ export class ContactComponent implements OnInit {
     };
 
     fetch(endpoint, requestOptions)
-    .then((response) => {
-      if (!response.ok) throw new Error("Error in fetch");
-      return response.json();
-    })
-    .then((response) => {
-      console.log("Email sent successfully!");
-      alert("Your message was sent. I will get back to you as soon as I can")
-    })
-    .catch((error) => {
-      console.log("An unkown error occured.");
-    });
+      .then((response) => {
+        if (!response.ok) throw new Error("Error in fetch");
+        return response.json();
+      })
+      .then((response) => {
+        console.log("Email sent successfully!");
+        alert("Your message was sent. I will get back to you as soon as I can");
+      })
+      .catch((error) => {
+        console.log("An unknown error occurred.");
+      });
   }
 
   private buildForm(): void {
     this.form = this.formBuilder.group({
-      name: this.formBuilder.control(null),
-      email: this.formBuilder.control(null),
-      message: this.formBuilder.control(null),
+      name: [null, Validators.required],
+      email: [null, [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
+      message: [null, Validators.required],
     });
   }
-
 }
